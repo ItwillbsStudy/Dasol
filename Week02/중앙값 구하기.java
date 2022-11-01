65227293ce99c8d37e91e2028b8f1d9500fa6bc6
dasol Lee <lxve6@naver.com>
2022-11-01 12:47:08 +0900

    Revert "1101"
    This reverts commit 79ce4b0881657462dffda58a352325a2a4ae4775.

--- Week02/중앙값 구하기.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-class Solution {
-    public int solution(int[] array) {
-        int answer = 0;
-        Arrays.sort(array);
-        return answer = array[(array.length-1)/2];
-    }
-}
