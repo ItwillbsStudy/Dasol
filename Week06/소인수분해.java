4040cc7a562a38f513db92e4ac640a8d0315f2c2
dasoLee <116339021+Losadee@users.noreply.github.com>
2022-11-29 16:37:08 +0900

    Update 소인수분해.java

--- Week06/소인수분해.java
+++ Week06/소인수분해.java
@@ -10,19 +10,21 @@ class Solution {
 				while(n % i != 0) {	// 나누어지지 않을 때까지 나눔
 					n/=2;
 				}
-			arr.add(i);
+			arr.add(i);	// 나누어진 소수를 list에 저장
 			}
 		}
-		
-		// list를 int[]로 변환
-		// Stream
-		// 일반적인 Stream 객체를 원시 Stream으로 바꾸거나
-		// 그 반대로 하는 작업이 필요한 경우 사용하며 복잡한 반복문, 조건문 등을 함수를 이용하여 한 줄에 쓸 수 있게 해줌..
-				
-		return arr.stream().mapToInt(i -> i).toArray();
-		// 리스트를 스트림으로 변환 후, map을 이용해서 intStream을 가져오고 (Integer -> int(primitive))
+	       return arr.stream().mapToInt(i -> i).toArray();	// 리스트 출력
+	       // 리스트를 스트림으로 변환 후, map을 이용해서 intStream을 가져오고 (Integer -> int(primitive))
 		// 그 후에 toArray()를 통해 배열로 만드는 방법이다.
 		// i -> i 는 값을 변형 없이 그대로 사용한다는 걸 의미하는 것 같음,,^^..
+	    
+		/*
+		< list를 int[]로 변환 >
+		Stream :
+		일반적인 Stream 객체를 원시 Stream으로 바꾸거나
+		그 반대로 하는 작업이 필요한 경우 사용하며 복잡한 반복문, 조건문 등을 함수를 이용하여 한 줄에 쓸 수 있게 해줌..
+		*/	
+		
 	}
     // 소수인지 판별하는 메서드
 	public static boolean isPrime(int n) {
